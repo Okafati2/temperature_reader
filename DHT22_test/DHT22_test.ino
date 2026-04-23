c2557ecc6684e5ae811c0d93c0b349ebcbc9a2bc
Okafati2 <odeh.kafati2@gmail.com>
2026-04-22 19:01:40 -0600

    Phase 2 - WiFi connectrion working with secure config file

--- DHT22_test/DHT22_test.ino
+++ DHT22_test/DHT22_test.ino
@@ -1,33 +1,21 @@
-#include <DHT.h>
-
-#define DHTPIN 4        // GPIO pin connected to DATA
-#define DHTTYPE DHT22   // sensor model
-
-DHT dht(DHTPIN, DHTTYPE);
+#include <WiFi.h>
+#include "config.h"
 
 void setup() {
   Serial.begin(115200);
-  dht.begin();
-  Serial.println("DHT22 sensor starting...");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected!");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  delay(2000); // wait 2 seconds between readings
-
-  float humidity = dht.readHumidity();
-  float temperature = dht.readTemperature();
-
-  if (isnan(humidity) || isnan(temperature)) {
-    Serial.println("Failed to read from DHT22 sensor!");
-    return;
-  }
-
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println(" °C");
-
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
-  Serial.println("---");
 }
